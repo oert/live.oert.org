9c4b3a9366126d86a3c7f93fdfa0bfdf7965a098
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2013-12-29 18:04:04 +0800

    Add language selector to mashead.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+(function() {
+  var langsel = document.getElementById('langsel');
+  var url = window.location.href;
+  if (url.indexOf('/es/') != -1) {
+    document.getElementById('lang-es').className = 'active';
+  }
+  else if (url.indexOf('/en-US/') != -1) {
+    document.getElementById('lang-en').className = 'active';
+  }
+})();
